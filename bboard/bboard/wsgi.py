c6bdb9b30519217e44a7719edd7c8c1903e72cf5
alexsmit75 <alexsmit75@list.ru>
2023-08-03 11:23:14 +0700

    v1c

---
+++ bboard/bboard/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for bboard project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+path = 'E:\skillfactory\bboard\bboard\bboard'
+if path not in sys.path:
+    sys.path.append(path)
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bboard.settings')
+
+application = get_wsgi_application()
